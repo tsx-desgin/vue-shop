2fba57b6afe255ae27c210fbdb7c8ea66c9eaf51
tsx-desgin <2928831850@qq.com>
2020-04-24 17:37:05 +0800

    分类

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -1,25 +1,64 @@
 <template>
 <div>
-    <Head title="分类" :back="backUrl"></Head>
-    <search-bar></search-bar>
-    <com-footer></com-footer>
+    <Head title="分类" :back="backUrl" class="header"></Head>
+    <search-bar class="search"></search-bar>
+    <div class="content">
+        <div class="content-left">
+            <category :content="category"></category>
+        </div>
+        <div class="content-right">
+            <sub-category :content="subCategory"></sub-category>
+        </div>
+    </div>
+    <com-footer ref="footer" class="footer"></com-footer>
 </div>
 </template>
 <script>
 import Head from "@/components/head"
 import comFooter from "@/components/footer";
 import SearchBar from "@/components/searchBAr"
+import category from "./category";
+import subCategory from "./subCategory";
 export default {
     components:{
         Head,
         SearchBar,
         comFooter,
+        category,
+        subCategory,
     },
     data(){
         return{
             backUrl:'',
+            category:[],//一级分类
+            subCategory:[],//子分类
+            catId:0,
         }
     },
+    mounted(){
+        this.getCategory()
+        const wh=document.documentElement.offsetHeight;
+        const head_h=document.querySelector('.header').offsetHeight;
+        const search_h=document.querySelector('.search').offsetHeight;
+        const footer_h=document.querySelector('.footer').offsetHeight;
+        const contentCon_h=wh-head_h-search_h-footer_h+'px';
+        document.querySelector('.content').style.height=contentCon_h;
+    },
+    methods:{
+        // 获取一级分类
+        async getCategory(){
+            const category=await this.axios.get('api/goods_category?type=1');
+            if(category.length>0){
+                this.getsubCategory(category[0].cat_id)
+                this.category=category;
+            }
+        },
+        // 获取子分类
+        async getsubCategory(catId){
+            this.subCategory=await this.axios.get('api/goods_category?type=1&cat_id='+catId);
+        },
+    },
+    // 导航守卫
     beforeRouteEnter (to, from, next) {
         next(vm=>{
         // 通过 `vm` 访问组件实例
@@ -30,4 +69,9 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import "~@/assets/scss/global";
+.content{
+    width:100%;
+    background: forestgreen;
+    margin-top: $head-h+$search-h;
+}
 </style>
